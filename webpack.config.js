325c4073453b0d48a9e9976cc380047929d812c3
GagnDeep <dp.gagn@gmail.com>
2021-10-18 15:20:29 +0530

    added global this

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ const config = {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+    globalObject: "this"
   },
   devtool: 'nosources-source-map',
   externals: {
