72d9d758235cf77787f761f59a28470b05749ac3
GagnDeep <dp.gagn@gmail.com>
2021-10-18 12:30:07 +0530

    displaying only days that have been added

--- src/views/BlogView.ts
+++ src/views/BlogView.ts
@@ -4,44 +4,106 @@ import dayjs, {extend} from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import path from 'path';
-import btoa from 'btoa';
+import { getGlobalStoreContext } from './../helpers/context';
+import atob from 'atob'
 
 extend(localizedFormat);
 extend(relativeTime);
 
+export type ViewProvider = "week" | "daily" | "monthly";
 export class BlogViewProvider implements vscode.TreeDataProvider<BlogItem> {
-	constructor(private context: ExtensionContext) { }
+	constructor(private context: ExtensionContext, private type?: ViewProvider) { }
 
-	getTreeItem(element: BlogItem): vscode.TreeItem {
+	getTreeItem(element: BlogItem | CollapsibleItem): vscode.TreeItem {
+
+		if(element instanceof CollapsibleItem){
+
+		}
 
 		return element;
 	}
 
-	getChildren(element?: BlogItem): Thenable<BlogItem[]> {
+	getChildren(element?: BlogItem | CollapsibleItem): Thenable<BlogItem[]> {
 		if(!element){
+
 			return Promise.resolve(this.getDays(0, 16));
 		}else{
 			return Promise.resolve([]);
 		}
 	}
 
+	getAllDaysWithContent(){
+		const store = getGlobalStoreContext();
+		return store.keys.map(item =>{
+			try{
+				return atob(item)
+			}catch(err){
+				return false
+			}
+		}).filter(item =>item.match(/.md$/));
+	}
+
 	getDays(start: number, end: number){
 
-		let days = [];
-
-		for(let i = start ; i< end; i++){
-			const d = dayjs().subtract(i, "d").format("LL");
-			days.push(d);
+		let days = this.getAllDaysWithContent();
+	
+		if(!days[0] || !days[0].includes(this.getTodayStr())){
+			days = [this.getTodayStr, ...days];
 		}
 		return days.map(day => {
 			return new BlogItem({
-				label: day + ".md",
+				label: day,
 				tooltip: `Blob created at ${day}`,
 				collapsibleState: vscode.TreeItemCollapsibleState.None
 			});
 		});
 	}
-	
+
+	getTodayStr(){
+		return dayjs().format("LL") + ".md";
+	}
+
+	getRoot(){
+		switch(this.type){
+			case "daily": return this.getAllDays();
+			case "monthly": return this.getMonthly();
+		}
+	}
+
+	getAllDays(){
+
+	}
+
+	getMonthly(){
+
+	}
+
+	getWeekly(){
+
+	}
+}
+
+class CollapsibleItem extends vscode.TreeItem {
+
+	public type: string;
+	constructor({
+		label,
+		description,
+		collapsibleState = vscode.TreeItemCollapsibleState.Collapsed,
+		tooltip = "tooltip",
+		type
+	}: {
+		label: string,
+		description?: string,
+		collapsibleState: vscode.TreeItemCollapsibleState,
+		tooltip?: string,
+		type?: string
+	}) {
+		super(label, collapsibleState);
+		this.tooltip = tooltip;
+		this.description = description;
+		this.type = type;
+	}
 }
 
 class BlogItem extends vscode.TreeItem {
