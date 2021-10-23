0509fca969b469d728cc5d71a60baf2217c92bab
GagnDeep <dp.gagn@gmail.com>
2021-10-23 10:27:24 +0530

    feat(dashboard): open dashboard when broadcast event fires

--- src/helpers/broadcast.ts
+++ src/helpers/broadcast.ts
@@ -1,11 +1,16 @@
 
-import { ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
 const CHANNEL_NAME = "gito-broadcast-channel"
 
-export const broadcastMessageHandler = (context: ExtensionContext) => {
+export const broadcastMessageHandler = (context: vscode.ExtensionContext) => {
 	const channel = new BroadcastChannel(CHANNEL_NAME);
 	channel.addEventListener('message', (event) => {
-		console.log({event});
-		console.log("homepage button triggered");
+		switch(event.data){
+			case "open-dashboard":{
+				vscode.commands.executeCommand("gito-new.start-cat-coding");
+				return;
+			}
+		}
 	});
+ 
 };
