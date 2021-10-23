71241c7a213e6792a4acd4a6ba0a46875e0d86d7
GagnDeep <dp.gagn@gmail.com>
2021-10-23 10:28:08 +0530

    chore(dashboard): added console.log for event data to test

--- src/helpers/broadcast.ts
+++ src/helpers/broadcast.ts
@@ -5,6 +5,7 @@ const CHANNEL_NAME = "gito-broadcast-channel"
 export const broadcastMessageHandler = (context: vscode.ExtensionContext) => {
 	const channel = new BroadcastChannel(CHANNEL_NAME);
 	channel.addEventListener('message', (event) => {
+		console.log({eventData: event.data});
 		switch(event.data){
 			case "open-dashboard":{
 				vscode.commands.executeCommand("gito-new.start-cat-coding");
